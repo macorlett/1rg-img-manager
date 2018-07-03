f56e4d67c98fd553c9e0cda9b9976792908691a9
Michael <macorlett@gmail.com>
2018-07-04 11:03:48 +1200

    updated with die alternative

--- php/config.php
+++ php/config.php
@@ -3,5 +3,5 @@
 	define('DB_USERNAME','website');
 	define('DB_PASSWORD', 'ElfyEa0SUi6s69ZG');
 	define('DB_DATABASE', 'db_img_manager');
-	$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+	$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE) or die("Server Error");
 ?>

b5db6a8bae8aebca8e236b0544d144a3207df17b
Michael <macorlett@gmail.com>
2018-07-04 14:33:25 +1200

    fix 05

--- php/session.php
+++ php/session.php
@@ -1,5 +1,5 @@
 <?php
-   //include('config.php');
+   include('config.php');
    session_start();
    
    $user= $_SESSION['user'];
--- upload.php
+++ upload.php
@@ -1,8 +1,5 @@
 <?php
-	include('config.php');
-	include('session.php');
-
-	$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE) or die("Server Error");
+	include('php/session.php');
 
 	if($_SERVER['REQUEST_METHOD']=="POST"){
 		

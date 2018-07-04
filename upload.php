2b4be81a3e6449eb1a61c0fd94ae18e76166c5a3
Michael <macorlett@gmail.com>
2018-07-04 14:31:54 +1200

    fix 04

--- upload.php
+++ upload.php
@@ -2,6 +2,7 @@
 	include('config.php');
 	include('session.php');
 
+	$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE) or die("Server Error");
 
 	if($_SERVER['REQUEST_METHOD']=="POST"){
 		

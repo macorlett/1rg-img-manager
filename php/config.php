5a2810cac9dfed563f89ae63db2d738224062d6c
Michael <macorlett@gmail.com>
2018-07-04 14:36:56 +1200

    re upload

--- php/config.php
+++ php/config.php
@@ -4,4 +4,5 @@
 	define('DB_PASSWORD', 'ElfyEa0SUi6s69ZG');
 	define('DB_DATABASE', 'db_img_manager');
 	$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE) or die("Server Error");
+	
 ?>
